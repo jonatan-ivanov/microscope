b8a0311429e3ec3458831e3caee6fa2b5928f222
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2017-02-19 16:11:09 -0800

    Using eureka-client instead of the default spring-boot admin client

--- src/main/java/microscope/Microscope.java
+++ src/main/java/microscope/Microscope.java
@@ -3,11 +3,13 @@ package microscope;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Jonatan Ivanov
  */
 @EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class Microscope {
     public static void main(String[] args) {
