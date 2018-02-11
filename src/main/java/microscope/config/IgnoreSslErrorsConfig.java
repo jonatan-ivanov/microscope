e849deb796fd93ba518d00aa4aabc54420f4795d
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2018-02-10 17:33:13 -0800

    Adding support to ignore ssl errors

---
+++ src/main/java/microscope/config/IgnoreSslErrorsConfig.java
@@ -0,0 +1,33 @@
+package microscope.config;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Jonatan Ivanov
+ */
+@Configuration
+@ConditionalOnProperty(name = "spring.boot.admin.discovery.ignore-ssl-errors")
+public class IgnoreSslErrorsConfig {
+    @Bean
+    public RestTemplateBuilder restTemplateBuilder() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (chain, authType) -> true).build();
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setSSLSocketFactory(new SSLConnectionSocketFactory(sslContext, (hostname, session) -> true))
+            .build();
+
+        return new RestTemplateBuilder().requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+    }
+}
