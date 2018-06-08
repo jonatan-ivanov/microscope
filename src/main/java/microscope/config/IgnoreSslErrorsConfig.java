74610f6134e93141605443dd14cb78fed9acfa5f
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2018-06-07 19:29:53 -0700

    Updating gradle and libs

--- src/main/java/microscope/config/IgnoreSslErrorsConfig.java
+++ src/main/java/microscope/config/IgnoreSslErrorsConfig.java
@@ -4,12 +4,15 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
+import javax.annotation.PostConstruct;
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -21,6 +24,8 @@ import java.security.NoSuchAlgorithmException;
 @Configuration
 @ConditionalOnProperty("spring.boot.admin.discovery.ignore-ssl-errors")
 public class IgnoreSslErrorsConfig {
+    @Autowired private ZuulProperties zuulProperties;
+
     @Bean
     public RestTemplateBuilder restTemplateBuilder() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (chain, authType) -> true).build();
@@ -29,5 +34,10 @@ public class IgnoreSslErrorsConfig {
             .build();
 
         return new RestTemplateBuilder().requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+    }
+
+    @PostConstruct
+    public void disableZuulSslHostnameValidation() {
+        zuulProperties.setSslHostnameValidationEnabled(false);
     }
 }
